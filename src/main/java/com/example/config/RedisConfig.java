55845e9960739a370ba83629cb37191f81b56b7e
ImportantOtter <importantotter@gmail.com>
2023-06-09 20:41:54 +0300

    added in RedisConfig method listenExpiredEvents

--- src/main/java/com/example/config/RedisConfig.java
+++ src/main/java/com/example/config/RedisConfig.java
@@ -1,27 +1,43 @@
 package com.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.Jedis;
 
 @Configuration
 public class RedisConfig {
 
+    @Autowired
+    private RedisExpirationListener redisExpirationListener;
+
+    private static final String REDIS_HOST = "localhost";
+
+    private static final int REDIS_PORT = 6379;
+
+    public void listenExpiredEvents() {
+        new Thread(() -> {
+            Jedis jedis = new Jedis(REDIS_HOST, REDIS_PORT);
+            RedisExpirationListener listener = new RedisExpirationListener();
+            jedis.psubscribe(listener, "__keyevent@*__:expired");
+        }).start();
+    }
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("localhost");
-        configuration.setPort(6379);
+        configuration.setHostName(REDIS_HOST);
+        configuration.setPort(REDIS_PORT);
         return new JedisConnectionFactory(configuration);
     }
 
     @Bean
-    public RedisTemplate<String, Object> template(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Object> template() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
