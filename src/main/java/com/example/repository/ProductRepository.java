63e66622bd1ef8f4125a8531e6372d40e86122c1
ImportantOtter <importantotter@gmail.com>
2023-06-09 20:44:38 +0300

    added sample RedisExpirationListener in ProductRepository class

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package com.example.repository;
 
+import com.example.config.RedisExpirationListener;
 import com.example.model.Product;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Repository
 public class ProductRepository {
@@ -14,15 +15,22 @@ public class ProductRepository {
     private static final long EXPIRATION_TIMEOUT = 5;
 
     private RedisTemplate redisTemplate;
+    private RedisExpirationListener expirationListener;
 
-    public ProductRepository(RedisTemplate redisTemplate) {
+    public ProductRepository(RedisTemplate redisTemplate, RedisExpirationListener expirationListener) {
         this.redisTemplate = redisTemplate;
+        this.expirationListener = expirationListener;
     }
 
     public Product saveProduct(Product product) {
-        redisTemplate.opsForHash().put(HASH_KEY, product.getId(), product);
-        redisTemplate.expire(HASH_KEY, EXPIRATION_TIMEOUT, TimeUnit.SECONDS);
+        Jedis jedis = new Jedis("localhost");
+        jedis.hset(HASH_KEY, String.valueOf(product.getId()), product.toString());
 
+        productInfo(product, jedis);
+
+        jedis.expire(HASH_KEY, EXPIRATION_TIMEOUT);
+
+        jedis.close();
         return product;
     }
 
@@ -37,5 +45,10 @@ public class ProductRepository {
     public String deleteProduct(int id) {
         redisTemplate.opsForHash().delete(HASH_KEY, id);
         return "product removed";
+    }
+
+    private static void productInfo(Product product, Jedis jedis) {
+        String productInfo = jedis.hget(HASH_KEY, String.valueOf(product.getId()));
+        System.out.println("Product info before expire: " + productInfo);
     }
 }
